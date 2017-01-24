40b2c5c2917fbcb7b95b75fbaa885b5b42373ce6
mxhaupt <maximilian.haupt@burda.com>
2017-01-24 17:49:53 +0100

    Added initial worker definition.

---
+++ src/Plugin/QueueWorker/RenderWorker.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\render_queue\Plugin\QueueWorker;
+
+use \Drupal\Core\Queue\QueueWorkerBase;
+
+class RenderWorker extends QueueWorkerBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function processItem($data) {
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder($data['type']);
+    $enabled_view_modes = \Drupal::service('entity_display.repository')
+      ->getViewModeOptionsByBundle($data['type'], $data['bundle']);
+    $renderer = \Drupal::service('renderer')->render($element);
+    $entity \Drupal::entityTypeManager()
+      ->getStorage($data['type'])->load($data['id']);
+    foreach ($enabled_view_modes as $display_id => $label) {
+      // TODO View per language.
+      $view = $view_builder->view($entity, $display_id);
+      $renderer->render($view);
+    }
+  }
+}
